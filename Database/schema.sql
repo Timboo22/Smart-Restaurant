b3ee4ccd41352fed420577b0be9430a26039c7f4
Felix <felix.polleit@cgi.com>
2026-09-01 13:39:22 +0200

    ref: add FK constraints

--- Database/schema.sql
+++ Database/schema.sql
@@ -5,32 +5,10 @@
     rolle varchar(50) NOT NULL 
 );
 
-CREATE TABLE status_log(
-    status_log_id SERIAL PRIMARY KEY ,
-    bestellung_id INTEGER NOT NULL ,
-    mitarbeiter_id INTEGER NOT NULL ,
-    status varchar(50) NOT NULL ,
-    zeitpunkt timestamp NOT NULL 
-);
-
-CREATE TABLE bestellung(
-    bestellung_id SERIAL PRIMARY KEY ,
-    tisch_id INTEGER NOT NULL ,
-    status varchar(50) NOT NULL ,
-    zeitpunkt timestamp NOT NULL 
-);
-
 CREATE TABLE tisch(
     tisch_id SERIAL PRIMARY KEY ,
     plaetze INTEGER NOT NULL ,
     status BOOLEAN NOT NULL
-);
-
-CREATE TABLE bestellposition (
-    bestellposition_id SERIAL PRIMARY KEY ,
-    bestellung_id INTEGER NOT NULL,
-    aritkel_id INTEGER NOT NULL,
-    menge INTEGER NOT NULL
 );
 
 CREATE TABLE artikel (
@@ -40,19 +18,75 @@ CREATE TABLE artikel (
     kategorie varchar(50) NOT NULL
 );
 
-CREATE TABLE artikel_zutaten(
-    artikel_zutaten_id SERIAL PRIMARY KEY ,
-    zutaten_id INTEGER NOT NULL ,
-    menge INTEGER NOT NULL 
-);
-
 CREATE TABLE zutaten(
     zutaten_id SERIAL PRIMARY KEY ,
-    zutaten_name varchar(50) NOT NULL 
+    zutaten_name varchar(50) NOT NULL
+);
+
+CREATE TABLE status_log(
+    status_log_id SERIAL PRIMARY KEY ,
+    bestellung_id INTEGER NOT NULL ,
+    mitarbeiter_id INTEGER NOT NULL ,
+    status varchar(50) NOT NULL ,
+    zeitpunkt TIMESTAMP NOT NULL,
+
+    CONSTRAINT fk_status_log_bestellung
+        FOREIGN KEY (bestellung_id)
+        REFERENCES bestellung(bestellung_id),
+
+    CONSTRAINT fk_status_log_mitarbeiter
+        FOREIGN KEY (mitarbeiter_id)
+        REFERENCES mitarbeiter(mitarbeiter_id)
+);
+
+CREATE TABLE bestellung(
+    bestellung_id SERIAL PRIMARY KEY ,
+    tisch_id INTEGER NOT NULL ,
+    status varchar(50) NOT NULL ,
+    zeitpunkt timestamp NOT NULL,
+
+    CONSTRAINT fk_bestelung_tisch
+        FOREIGN KEY (tisch_id)
+        REFERENCES tisch(tisch_id)
+);
+
+CREATE TABLE bestellposition (
+    bestellposition_id SERIAL PRIMARY KEY ,
+    bestellung_id INTEGER NOT NULL,
+    artikel_id INTEGER NOT NULL,
+    menge INTEGER NOT NULL,
+
+    CONSTRAINT fk_bestellposition_bestellung
+        FOREIGN KEY (bestellung_id)
+        REFERENCES bestellung(bestellung_id),
+
+    CONSTRAINT fk_bestellposition_artikel
+        FOREIGN KEY (artikel_id)
+        REFERENCES artikel(artikel_id)                               
+);
+
+CREATE TABLE artikel_zutaten(
+    artikel_zutaten_id INTEGER NOT NULL ,
+    zutaten_id INTEGER NOT NULL ,
+    menge INTEGER NOT NULL,
+
+    PRIMARY KEY (artikel_id, zutaten_id),
+
+    CONSTRAINT fk_artikel_zutaten_artikel
+        FOREIGN KEY (artikel_id)
+        REFERENCES artikel(artikel_id),
+
+    CONSTRAINT fk_artikel_zutaten_zutat
+        FOREIGN KEY (zutaten_id)
+        REFERENCES zutaten(zutaten_id)
 );
 
 CREATE TABLE lager(
     zutaten_lager_id SERIAL PRIMARY KEY ,
     soll INTEGER NOT NULL ,
-    ist INTEGER NOT NULL 
+    ist INTEGER NOT NULL,
+
+    CONSTRAINT fk_lager_zutat
+        FOREIGN KEY (zutaten_id)
+        REFERENCES zutaten(zutaten_id)
 )
